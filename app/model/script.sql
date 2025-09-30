bfdc346d16a3fb2a295a490679d4b339474c8dee
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-09-29 22:26:33 -0300

    Create script.sql

---
+++ app/model/script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE db_aventura_pet;
+
+USE db_aventura_pet;
+
+CREATE TABLE usuario (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(100), 
+    tipo_usuario BOOLEAN
+);
+
+CREATE TABLE contato_usuario(
+    id_contato_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    telefone VARCHAR(20),
+    cep VARCHAR(8),
+    email VARCHAR(100),
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+CREATE TABLE password_hash(
+    id_password_hash INT PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INT,
+    password_hash VARCHAR(300),
+    ativo BOOLEAN,
+    data_criacao DATE,
+    data_inativacao DATE,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
