6c65297d2881e00153c802031d5963b6b13d3c39
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-10-09 19:45:04 -0300

    Update aventuraPetController.js
    atualizando rotina de insersao de pet e imagem do pet

--- app/controller/aventuraPetController.js
+++ app/controller/aventuraPetController.js
@@ -1,42 +1,51 @@
-const imagemPetModel = require('../model/models/imagemPetModel');
+const petUserModel = require('../model/models/petUserModel');
+const imagPetModel = require('../model/models/imgePetModel');
 const userModel = require('../model/models/userModel');
 const axios = require('axios')
 
 module.exports = {
-    index:function(req, res){
+    index: function (req, res) {
         let cep = req.session.userAutentication.dataContactUser.cep
         axios.get({
-            method:'get',
+            method: 'get',
             url: `https://www.cepaberto.com/api/v3/cep?cep=${cep}`,
-            data:{
-                headers:{'Authorization': 'Token token=be380c54fed0e26adf0f82bf75438313'}
+            data: {
+                headers: { 'Authorization': 'Token token=be380c54fed0e26adf0f82bf75438313' }
             }
-             
-        }).then(function(response){
-            
+
+        }).then(function (response) {
+
         })
 
-        /*
-        SELECT ST_Distance_Sphere(
-            POINT(-23.542298, -46.8848325),
-            POINT(-23.5421686, -46.8812042)
-)
-        */
-        res.render('aventura-pet/index', {fileName: 'main'});
+
+        res.render('aventura-pet/index', { fileName: 'main' });
     },
-    addPetPage: function(req, res){
-        
-        res.render('aventura-pet/index', {fileName:'add-pet'});
+    addPetPage: function (req, res) {
+
+        res.render('aventura-pet/index', { fileName: 'add-pet' });
     },
-    insertImgPet: async function(req, res){
+    insertImgPet: async function (req, res) {
         let idUser = req.session.userAutentication.dataUser.idUser;
-        for(let i = 0; i < req.body.imgpet.length; i++){
-            await imagemPetModel.create({
+
+        try {
+            const userPet = await petUserModel.create({
                 id_usuario: idUser,
-                name_pet: req.body.namepet,
-                imagem : req.body.namepet[i]
+                nome_pet: req.bode.namePet,
+                disponivel: true
             });
+
+            const idPetUser = userPet.id_user_pet;
+
+            for (let i = 0; i < req.body.imgpet.length; i++) {
+                await imagPetModel.create({
+                    id_user_pet: idPetUser,
+                    imagem: req.body.imgpet[i]
+                });
+            }
+        } catch (error) {
+
         }
+
 
     }
 }
